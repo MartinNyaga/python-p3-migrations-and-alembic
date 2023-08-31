49d2674fee4ef52aa737bfd830cc6d7d2d42d92d
Martin Nyaga <martin.nyaga@student.moringaschool.com>
2023-08-31 21:04:00 +0300

    completed assignment

---
+++ lib/migration/versions/b1efb0be842d_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: b1efb0be842d
+Revises: 
+Create Date: 2023-08-31 20:08:45.795055
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b1efb0be842d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
